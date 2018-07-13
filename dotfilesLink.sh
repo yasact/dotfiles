869bf92cbf60beacd4cb4b7aabd1907ab893b22d
yasact <yasunaga.akutsu@gmail.com>
2018-07-13 09:29:41 +0900

    add .profile fixed link syntax on dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/colors/ ~/.vim
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.profile ~/.profile
+
+mkdir -p ~/.vim
+ln -sf ~/dotfiles/colors ~/.vim/colors
