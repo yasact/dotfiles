06f9c362596b86d1017c6c8c6cb1384f68c94a75
yasact <yasunaga.akutsu@gmail.com>
2022-10-25 13:33:32 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,6 +2,7 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.profile ~/.profile
+ln -sf ~/dotfiles/.inputrc ~/.inputrc
 
 mkdir -p ~/.vim
 ln -sf ~/dotfiles/colors ~/.vim/colors
