2eac2f377dd439c4c26f497531424fe17b5caab5
yasact <yasunaga.akutsu@gmail.com>
2023-12-25 13:32:00 +0900

    Add new files and update existing files

--- link.sh
+++ link.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.profile ~/.profile
-ln -sf ~/dotfiles/.inputrc ~/.inputrc
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/bash/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/bash/.profile ~/.profile
+ln -sf ~/dotfiles/bash/.inputrc ~/.inputrc
+ln -sf ~/dotfiles/bash/.bash_aliases ~/.bash_aliases
 
 mkdir -p ~/.vim
 ln -sf ~/dotfiles/colors ~/.vim/colors
 
-mkdir -p ~/.config/nvim/lua
-ln -sf ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
-ln -sf ~/dotfiles/nvim/lua/base.lua ~/.config/nvim/lua/base.lua
-ln -sf ~/dotfiles/nvim/lua/keymaps.lua ~/.config/nvim/lua/keymaps.lua
-ln -sf ~/dotfiles/nvim/lua/options.lua ~/.config/nvim/lua/options.lua
-ln -sf ~/dotfiles/nvim/lua/plugins.lua ~/.config/nvim/lua/plugins.lua
-
+# mkdir -p ~/.config/nvim/lua
+# ln -sf ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+# ln -sf ~/dotfiles/nvim/lua/base.lua ~/.config/nvim/lua/base.lua
+# ln -sf ~/dotfiles/nvim/lua/keymaps.lua ~/.config/nvim/lua/keymaps.lua
+# ln -sf ~/dotfiles/nvim/lua/options.lua ~/.config/nvim/lua/options.lua
+# ln -sf ~/dotfiles/nvim/lua/plugins.lua ~/.config/nvim/lua/plugins.lua
