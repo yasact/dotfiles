ab9d3ea31d3a719375e3dbc4226b1b60c4f75494
YasAct <yasunaga.akutsu@gmail.com>
2024-01-26 13:57:56 +0900

    update

--- windows/AppSetup.ps1
+++ windows/AppSetup.ps1
@@ -1,77 +1,34 @@
-
 # winget
-# winget install AgileBits.1Password
-# winget install Amazon.Kindle
-# winget install AutoHotkey.AutoHotkey
-# winget install LGUG2Z.komorebi
-# winget install LGUG2Z.whkd
-# winget install Anki.Anki
-# winget install Brave.Brave
-# winget install Docker.DockerDesktop
-# winget install Dropbox.Dropbox
-# winget install Foxit.FoxitReader
-# winget install Git.Git
-# winget install Google.Chrome
-# winget install LibreWolf.LibreWolf
-# winget install Logitech.LGS
-# winget install Microsoft.WindowsTerminal
-# # winget install Mozilla.Thunderbird
-# # winget install Microsoft.OneDrive
-# winget install PhraseExpress.PhraseExpress
-# # winget install PostgreSQL.PostgreSQL
-# # winget install Debian.Debian
-# winget install vim.vim
-# # winget install Waterfox.Waterfox
-# # winget install Microsoft.VisualStudio.2022.Community
-# # winget install Microsoft.VisualStudio.2022.BuildTools
-# winget install OpenJS.NodeJS
-# winget install DeepL.DeepL
-# winget install SlackTechnologies.Slack
-# winget install Microsoft.SQLServerManagementStudio
-# # winget install Kensington.KensingtonWorks
-# winget install Neovim.Neovim
-# winget install Microsoft.PowerShell
-# # winget install Microsoft.VisualStudioCode.Insiders
-# winget install Microsoft.VisualStudioCode
-# winget install Python.Python.3.10
-# winget install Python.Launcher
-# winget install Microsoft.PowerShell
-# winget install Microsoft.PowerToys
-# winget install 7zip.7zip
-
-
-
-
 $applications = @(
+    "7zip.7zip"
     "AgileBits.1Password"
     "Amazon.Kindle"
-    "AutoHotkey.AutoHotkey"
-    "LGUG2Z.komorebi"
-    "LGUG2Z.whkd"
     "Anki.Anki"
+    # "AutoHotkey.AutoHotkey"
     "Brave.Brave"
+    "DeepL.DeepL"
     "Docker.DockerDesktop"
     "Dropbox.Dropbox"
     "Foxit.FoxitReader"
     "Git.Git"
     "Google.Chrome"
+    "LGUG2Z.komorebi"
+    "LGUG2Z.whkd"
     "LibreWolf.LibreWolf"
     "Logitech.LGS"
-    "Microsoft.WindowsTerminal"
-    "PhraseExpress.PhraseExpress"
-    "vim.vim"
-    "OpenJS.NodeJS"
-    "DeepL.DeepL"
-    "SlackTechnologies.Slack"
-    "Microsoft.SQLServerManagementStudio"
-    "Neovim.Neovim"
     "Microsoft.PowerShell"
-    "Microsoft.VisualStudioCode"
-    "Python.Python.3.10"
-    "Python.Launcher"
     "Microsoft.PowerShell"
     "Microsoft.PowerToys"
-    "7zip.7zip"
+    "Microsoft.SQLServerManagementStudio"
+    "Microsoft.VisualStudioCode"
+    "Microsoft.WindowsTerminal"
+    "Neovim.Neovim"
+    "OpenJS.NodeJS"
+    "PhraseExpress.PhraseExpress"
+    "Python.Launcher"
+    "Python.Python.3.10"
+    "SlackTechnologies.Slack"
+    "vim.vim"
 )
 
 foreach ($app in $applications)
@@ -83,8 +40,22 @@ foreach ($app in $applications)
 
 # scoop
 # install scoop
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-# Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+# Scoopの存在を確認
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue))
+{
+    try
+    {
+        # Scoopが存在しない場合、インストールを試みる
+        Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+        Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+    }
+    catch
+    {
+        # エラーが発生した場合の処理
+        Write-Error "Scoopのインストール中にエラーが発生しました: $_"
+        exit
+    }
+}
 
 # bucket
 scoop bucket add versions
